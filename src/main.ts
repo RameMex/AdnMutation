70c3b5b97c7da776e3e7aff079d3736cf20524f2
René Alberto Meza Escamilla <meza.rene@uabc.edu.mx>
2021-05-20 08:21:24 -0700

    Cambiando puerto

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+  var server_host = process.env.YOUR_HOST || '0.0.0.0';
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(server_port, server_host, function() {
+    console.log('Escuchando en el puerto:  %d', server_port);
+});
 }
 bootstrap();
